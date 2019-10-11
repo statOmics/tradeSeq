5f71651ac53d5136c06b54edf05d88b3b85e5673
koenvandenberge <koen.vandenberge@ugent.be>
2019-10-11 11:42:26 +0200

    few more pkg tests

--- tests/testthat/testConsistentOutput.R
+++ tests/testthat/testConsistentOutput.R
@@ -1,25 +1,6 @@
 context("Consistent tradeSeq output with different inputs.")
 
-n <- nrow(reducedDim(sds))
-G <- 100
-pseudotime <- slingPseudotime(sds, na=FALSE)
-cellWeights <- slingCurveWeights(sds)
-means <- matrix(rep(rlnorm(n=G, meanlog=4, sdlog=1), n),
-                nrow=G, ncol=n, byrow=FALSE)
-dispersions <- matrix(rep(runif(n=G, min=0.8, max=3), n),
-                      nrow=G, ncol=n, byrow=FALSE)
-# add pseudotime effects for a few
-id <- sample(1:100, 20)
-means[id,] <- sweep(means[id,],2,FUN="*",STATS=(pseudotime[,1]/50))
-# simulate NB counts
-counts <- matrix(rnbinom(n=G*n, mu=means, size=1/dispersions), nrow=G, ncol=n)
 
-
-# fitGAM tests
-sdsFit <- tradeSeq::fitGAM(counts, sds, nknots=3, verbose=FALSE, parallel=FALSE)
-listFit <- tradeSeq::fitGAM(counts, pseudotime = pseudotime,
-                            cellWeights = cellWeights, nknots = 3,
-                            verbose = FALSE, parallel = FALSE)
 # extract coefficients
 betasds <- as.matrix(rowData(sdsFit)$tradeSeq$beta)
 betaList <- do.call(rbind, lapply(listFit, function(m) coef(m)))
