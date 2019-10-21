8ae40600392ee3fd46697dd4b2d2e64cb8e93322
koenvandenberge <koen.vandenberge@ugent.be>
2019-10-21 15:13:11 +0200

    update docs

--- R/data.R
+++ R/data.R
@@ -14,3 +14,43 @@
 #'
 #' @source \url{https://bioconductor.org/packages/release/bioc/html/slingshot.html}
 "sds"
+
+#' A SlingshotDataset object, used in the package vignette.
+#'
+#' This dataset contains the Slingshot trajectory from the data described in
+#' Paul et al. (2015).
+#'
+#' @references
+#' Franziska Paul, Yaara Arkin, Amir Giladi, DiegoAdhemar Jaitin, Ephraim Kenigsberg, Hadas KerenShaul, Deborah Winter, David Lara-Astiaso, Meital Gury, Assaf Weiner, Eyal David, Nadav Cohen,
+#' FeliciaKathrineBratt Lauridsen, Simon Haas, Andreas Schlitzer, Alexander Mildner, Florent Ginhoux,
+#' Steen Jung, Andreas Trumpp, BoTorben Porse, Amos Tanay, and Ido Amit. Transcriptional Heterogeneity and Lineage Commitment in Myeloid Progenitors. Cell, 163(7):1663–1677, 12 2015. ISSN 0092-
+#' 8674. doi: 10.1016/J.CELL.2015.11.013. URL https://www.sciencedirect.com/science/article/
+#' ii/S0092867415014932?via%3Dihub#app3.
+"crv"
+
+#' A count matrix, used in the package vignette.
+#'
+#' This object contains the gene expression counts from the data described in
+#' Paul et al. (2015).
+#'
+#' #' @references
+#' Franziska Paul, Yaara Arkin, Amir Giladi, DiegoAdhemar Jaitin, Ephraim Kenigsberg, Hadas KerenShaul, Deborah Winter, David Lara-Astiaso, Meital Gury, Assaf Weiner, Eyal David, Nadav Cohen,
+#' FeliciaKathrineBratt Lauridsen, Simon Haas, Andreas Schlitzer, Alexander Mildner, Florent Ginhoux,
+#' Steen Jung, Andreas Trumpp, BoTorben Porse, Amos Tanay, and Ido Amit. Transcriptional Heterogeneity and Lineage Commitment in Myeloid Progenitors. Cell, 163(7):1663–1677, 12 2015. ISSN 0092-
+#' 8674. doi: 10.1016/J.CELL.2015.11.013. URL https://www.sciencedirect.com/science/article/
+#' ii/S0092867415014932?via%3Dihub#app3.
+"counts"
+
+
+#' A vector defining cell types, used in the package vignette.
+#'
+#' This object contains a vector that define the cell type for each cell in
+#' the data described in Paul et al. (2015).
+#'
+#' #' @references
+#' Franziska Paul, Yaara Arkin, Amir Giladi, DiegoAdhemar Jaitin, Ephraim Kenigsberg, Hadas KerenShaul, Deborah Winter, David Lara-Astiaso, Meital Gury, Assaf Weiner, Eyal David, Nadav Cohen,
+#' FeliciaKathrineBratt Lauridsen, Simon Haas, Andreas Schlitzer, Alexander Mildner, Florent Ginhoux,
+#' Steen Jung, Andreas Trumpp, BoTorben Porse, Amos Tanay, and Ido Amit. Transcriptional Heterogeneity and Lineage Commitment in Myeloid Progenitors. Cell, 163(7):1663–1677, 12 2015. ISSN 0092-
+#' 8674. doi: 10.1016/J.CELL.2015.11.013. URL https://www.sciencedirect.com/science/article/
+#' ii/S0092867415014932?via%3Dihub#app3.
+"counts"
