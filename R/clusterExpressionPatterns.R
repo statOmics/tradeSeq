f79224a8616fa4190eb9dcb4fad8f2053a250085
Hector <hector.rouxdebezieux@berkeley.edu>
2020-01-14 14:51:29 +0000

    fixes #23

--- R/clusterExpressionPatterns.R
+++ R/clusterExpressionPatterns.R
@@ -38,6 +38,7 @@
     slingshotColData <- colData(models)$slingshot
     pseudotime <- slingshotColData[,grep(x = colnames(slingshotColData),
                                          pattern = "pseudotime")]
+    if (is.null(dim(pseudotime))) pseudotime <- matrix(pseudotime, ncol = 1)
     nCurves <- length(grep(x = colnames(dm), pattern = "t[1-9]"))
     betaMat <- rowData(models)$tradeSeq$beta[[1]]
     beta <- betaMat[id,]
