9d7663312fdb7a1b0fd9fa004253679904887c65
koenvandenberge <koen.vandenberge@ugent.be>
2020-02-24 15:21:18 -0800

    add tests for FC diffEnd and startVsEnd

--- data/loadDatasetsForTesting.R
+++ data/loadDatasetsForTesting.R
@@ -1,15 +1,21 @@
-### get list output, 2 lineages
-data(gamList, package = "tradeSeq")
+# ### get list output, 2 lineages
+# data(gamList, package = "tradeSeq")
+#
+# ### get sce output, 2 lineages
+# set.seed(8)
+# data(crv, package="tradeSeq")
+# data(countMatrix, package="tradeSeq")
+# sce2 <- fitGAM(counts = as.matrix(countMatrix),
+#                sds = crv,
+#                nknots = 5)
+#
+# ### sce output, 4 lineages
+# download.file("https://docs.google.com/uc?export=download&id=1GhjXcdNLcvukX5Mr68oElemoUxI15v-I",
+#               destfile="~/tmp/sce4.rds")
+# sce4 <- readRDS("~/tmp/sce4.rds")
 
-### get sce output, 2 lineages
-set.seed(8)
-data(crv, package="tradeSeq")
-data(countMatrix, package="tradeSeq")
-sce2 <- fitGAM(counts = as.matrix(countMatrix),
-               sds = crv,
-               nknots = 5)
+### list output, 4 lineages
+# download.file("https://docs.google.com/uc?export=download&id=1jOc6eTzxwq1GbNbAqXLO6VL9MuVnJWrV",
+#               destfile="~/tmp/list4.rds")
+# list <- readRDS("~/tmp/list4.rds")
 
-### sce output, 4 lineages
-download.file("https://docs.google.com/uc?export=download&id=1GhjXcdNLcvukX5Mr68oElemoUxI15v-I",
-              destfile="~/tmp/sce4.rds")
-sce4 <- readRDS("~/tmp/sce4.rds")
